c3cf2ac3e368fb0942603c72ba915e1fd994fa3b
srquev <ssharique38@gmail.com>
2024-10-28 12:16:33 +0530

    logout button right inn header

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ function Header() {
           <li><Link to="/">Home</Link></li>
           <li><Link to="/login">Login</Link></li>
           <li><Link to="/signup">Sign Up</Link></li>
-          <li><Link to="/" style={{ aligntext: 'right'}}>Logout</Link></li>
+          <li><Link to="/" style={{ display: 'flex', justifyContent: 'flex-end' }}>Logout</Link></li>
         </ul>
       </nav>
     </header>
